d45b1dc113977c95744a5660276171f2a2485b3d
Regan Poston <rsposton@users.noreply.github.com>
2021-03-13 12:02:34 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Trying Some Code Sandbox Magic</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <p>Did a fork to see a new commit.</p>
     </div>
   );
 }
